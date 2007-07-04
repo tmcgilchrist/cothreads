838389303f37a98999b407affbd4e09b42215d30
zheng_li <zheng_li@1a53509c-082f-0410-9405-baebebd3041d>
2007-07-04 22:02:23 +0000

    iThread primitives.

--- src/ithread.mli
+++ src/ithread.mli
@@ -3,31 +3,29 @@ type t
 val self : unit -> t
 val id : t -> int
 
-val create: ('a -> 'b) -> 'a -> t
 val exit: unit -> unit
 val kill: t -> unit
-val join: t -> unit
 
 
 (**/**)
 
 type 'a portal
 
-val portal: unit -> 'a portal
+val new_portal: unit -> 'a portal
+val create_portal: 'a portal -> unit
+val remove_portal: 'a portal -> unit
+
+
+(* Only root node can manipulate service *)
+val new_serv: 'a portal -> ('a -> unit) -> unit
+val del_serv: 'a portal -> ('a -> unit) -> unit
+val sub_serv: 'a portal -> ('a -> unit) -> ('a -> unit) -> unit
+
+
 val send: 'a -> 'a portal -> unit
 val recv: 'a portal -> 'a
 
-(* Only root node can register service *)
-val service: (([> ] as 'a)  -> unit) -> unit 
-val demand: 'a -> 'b portal -> 'b
-val command: 'a -> unit
-
-
-
-
-
-
-
-
-
-
+(*
+val demand: ([> ] as 'a) -> 'b portal -> 'b 
+val command: ([> ] as 'a) -> unit
+*)
