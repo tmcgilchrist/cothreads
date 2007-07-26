6eb40cd48f62a29bac8d000d99d5504cd0a97065
zheng_li <zheng_li@1a53509c-082f-0410-9405-baebebd3041d>
2007-07-26 19:41:13 +0000

    We should use more Unix.unlink rather than Sys.remove

--- trunk/example/ith_demo.ml
+++ trunk/example/ith_demo.ml
@@ -1,9 +1,17 @@
-open Ithread
+open Cothread
+
+let lk = Mutex.create ()
 
 let test_f () = 
+  let myid = id (self ()) in
   let r = Random.int 100 in
   for i = 0 to r do
-    test (Printf.sprintf "%dth test from %d\n" i (id (self ())))
+    test (Printf.sprintf "%dth test from %d\n" i myid);
+    while (not (Mutex.try_lock lk)) do 
+      Printf.printf "%d fail to get the mutex\n" myid;
+    done;
+    Printf.printf "%d finally get the mutex\n" myid;
+    Mutex.unlock lk
   done
 
 let ith_array = Array.init 7 (fun _ -> create test_f ()) 
